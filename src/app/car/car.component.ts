9120822e346d563bf738b961a689b2afabc947f7
Kateryna Maistrenko <kmais@softserveinc.com>
2019-03-01 17:56:14 +0200

    Added Car and Cars components, bootstrap

---
+++ src/app/car/car.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car',
+  templateUrl: './car.component.html',
+  styleUrls: ['./car.component.css']
+})
+export class CarComponent {
+
+  carName = 'Ford';
+  carYear = 2015;
+
+}
